7f968f1dae3ef99a6ac4fa961991a49197a8217c
Nicholas Mostacero <nickmostacero@gmail.com>
2014-10-29 00:08:45 -0700

    end of milestone 6. 10/29

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -1,19 +1,19 @@
+/*game variables*/
 var snake;
 var snakeLength;
 var snakeSize;
 
+var food;
+
 var context;
 var screnWidth;
 var screenHeight;
-
+/*Executing game code*/
 gameInitialize();
 snakeInitialize();
-gameDraw();
-snakeDraw();
-snakeUpdate();
-snakeDraw();
-snakeUpdate();
-snakeDraw();
+foodInitialize();
+setInterval(gameLoop, 1000/30);
+/*game functions*/
 function gameInitialize() {
     var canvas = document.getElementById("game-screen");
    context = canvas.getContext ("2d");
@@ -26,20 +26,24 @@ function gameInitialize() {
 }
 
 function gameLoop() {
-    
+    gameDraw();
+    snakeUpdate();
+    snakeDraw();
+    foodDraw();
 }
 
 function gameDraw() {
     context.fillStyle = "rgb(180, 250, 213)";
     context.fillRect(0, 0, screenWidth, screenHeight);
+    
 }
-
+/*snake functions*/
 function snakeInitialize () {
  snake = [];
- snakeLength: 5;
- snakeSize: 20;
+ snakeLength= 5;
+ snakeSize= 20;
  
- for (var index = snakeLength - 1; index >=0; index--) {
+ for (var index = snakeLength -1; index >=0; index--) {
      snake.push( {
          x: index,
          y: 0
@@ -48,7 +52,7 @@ function snakeInitialize () {
 }
 
 function snakeDraw () {
- for (var index = 0; index < snakelength; index++) {
+ for (var index = 0; index < snake.length; index++) {
      context.fillStyle = "white";
      context.fillRect(snake[index].x * snakeSize, snake[index].y * snakeSize, snakeSize, snakeSize);
  }
@@ -64,4 +68,25 @@ function snakeUpdate () {
     snakeTail.x = snakeHeadX;
     snakeTail.y = snakeHeadY;
     snake.unshift(snakeTail);
+}
+/*food functions*/
+function foodInitialize(){
+    food = {
+        x: 0,
+        y: 0
+    };
+    setFoodPosition();
+}
+
+function foodDraw(){
+    context.fillStyle = "white";
+    context.fillRect(food.x, food.y, snakeSize, snakeSize);
+}
+
+function setFoodPosition(){
+    var randomX = Math.floor(Math.random() * screenWidth);
+    var randomY = Math.floor(Math.random() * screenHeight);
+    
+    food.x = randomX;
+    food.y = randomY;
 }
